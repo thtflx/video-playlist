5e4c77fb1952b11189aa6b198d14f232ba502f77
thtflx <azizsattorovthtflx@gmail.com>
2023-01-16 00:01:18 +0300

    Add under the hood code

---
+++ app.js
@@ -0,0 +1,18 @@
+const listVideo = document.querySelectorAll('.video-list .vid');
+const mainVideo = document.querySelector('.main-video video');
+const title = document.querySelector('.main-video .title');
+
+listVideo.forEach(video => {
+    video.onclick = () => {
+        listVideo.forEach((vid) => {
+            vid.classList.remove('active');
+        });
+        video.classList.add('active');
+        if (video.classList.contains('active')) {
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text;
+        };
+    };
+});
